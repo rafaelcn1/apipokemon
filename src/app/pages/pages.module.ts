893b460eaac245dd8441e7db2143135aa0ab2235
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-20 15:29:30 -0300

    feat: criação componente compartilhada poke header

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './routing.module';
 import { DetailsComponent } from './details/details.component'; //MOdule Routing
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -13,7 +14,8 @@ import { DetailsComponent } from './details/details.component'; //MOdule Routing
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 
-
-
 @NgModule({
-  declarations: [
-    PokeHeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PokeHeaderComponent],
+  exports: [PokeHeaderComponent],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
