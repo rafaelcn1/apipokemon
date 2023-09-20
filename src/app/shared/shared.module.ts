61520eff1e917c2da33732621a268113a41acede
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-20 15:03:32 -0300

    feat: criação component shared/poke-header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PokeHeaderComponent } from './poke-header/poke-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PokeHeaderComponent
+  ],
   imports: [
     CommonModule
   ]
