0d363071f2cbb31d8b622491d51c6a9542b314b0
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-20 15:31:11 -0300

    feat: criação componete poke-search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
 
 @NgModule({
-  declarations: [PokeHeaderComponent],
+  declarations: [PokeHeaderComponent, PokeSearchComponent],
   exports: [PokeHeaderComponent],
   imports: [CommonModule],
 })
