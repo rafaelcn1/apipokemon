fedbfa35c1c68a3acc52f2ee91033d7d713c1fb0
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-25 13:51:23 -0300

    Integração back end com a api

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -9,6 +9,6 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 export class PokeListComponent implements OnInit {
   constructor(private pokeApiService: PokeApiService) {}
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemon.subscribe((res) => res);
+    this.pokeApiService.apiListAllPokemon.subscribe((res) => console.log(res));
   }
 }
