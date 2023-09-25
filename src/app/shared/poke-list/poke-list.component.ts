b6b902bb9c5dfada3e57f72b12d7d57dd1914327
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-25 15:22:42 -0300

    fix: ajuste no filter dos pokemons

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,11 +8,20 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 })
 export class PokeListComponent implements OnInit {
   public getAllPokemons: any;
+  private setAllPokemons: any;
   constructor(private pokeApiService: PokeApiService) {}
   ngOnInit(): void {
-    this.pokeApiService.apiListAllPokemon.subscribe(res => {
-      this.getAllPokemons = res.results;
-      console.log(this.getAllPokemons);
+    this.pokeApiService.apiListAllPokemon.subscribe((res) => {
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
     });
+  }
+
+  public recuperarPokeSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
   }
 }
